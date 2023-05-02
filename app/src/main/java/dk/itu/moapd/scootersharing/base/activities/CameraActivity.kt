f0c78d79a40f295e77caabae3d1a495c89aace2c
COHL7500 <cohl7500@gmail.com>
2023-05-02 12:13:21 +0200

    Add CameraFragment.kt

---
+++ app/src/main/java/dk/itu/moapd/scootersharing/base/activities/CameraActivity.kt
@@ -0,0 +1,53 @@
+/*
+ * MIT License
+ * Copyright (c) 2023 Bastjan Sejberg
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dk.itu.moapd.scootersharing.base.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dk.itu.moapd.scootersharing.base.R
+import dk.itu.moapd.scootersharing.base.databinding.ActivityCameraBinding
+import dk.itu.moapd.scootersharing.base.databinding.ActivityListRidesBinding
+import dk.itu.moapd.scootersharing.base.fragments.StartRideFragment
+import dk.itu.moapd.scootersharing.base.databinding.ActivityStartRideBinding
+import dk.itu.moapd.scootersharing.base.databinding.ActivityUpdateRideBinding
+import dk.itu.moapd.scootersharing.base.fragments.ListRidesFragment
+
+class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+
+        val urFragment = ListRidesFragment()
+        val view = binding.root
+
+        setContentView(view)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment_container_view, urFragment)
+            .commit()
+    }
+}
+
---
+++ app/src/main/java/dk/itu/moapd/scootersharing/base/fragments/CameraFragment.kt
@@ -0,0 +1,79 @@
+/*
+ * MIT License
+ * Copyright (c) 2023 Bastjan Sejberg
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dk.itu.moapd.scootersharing.base.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import dk.itu.moapd.scootersharing.base.*
+import dk.itu.moapd.scootersharing.base.adapters.CustomFirebaseAdapter
+import dk.itu.moapd.scootersharing.base.databinding.FragmentCameraBinding
+import dk.itu.moapd.scootersharing.base.databinding.FragmentListRidesBinding
+import dk.itu.moapd.scootersharing.base.models.Scooter
+
+class CameraFragment : Fragment() {
+
+    private var _binding: FragmentCameraBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+
+        }
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
